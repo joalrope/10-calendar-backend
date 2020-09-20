4d5ddbec7a114b781689fec967eb13be4dca05fe
Jose Rodriguez <joalrope@gmail.com>
2020-09-20 18:25:17 -0400

    Frontend Reafct. agregado

---
+++ public/precache-manifest.20fa19c6c4027b4fb55018c74a217a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f874a9a17ab425cc5fca766e9f35a966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786d1120d9181baf474f",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "c074d54d22bc79e53e2c",
+    "url": "/static/css/main.fe16dea6.chunk.css"
+  },
+  {
+    "revision": "786d1120d9181baf474f",
+    "url": "/static/js/2.c5158c05.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.c5158c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c074d54d22bc79e53e2c",
+    "url": "/static/js/main.24422b1f.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  }
+]);
